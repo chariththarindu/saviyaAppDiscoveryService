a7f93f63957f22ea173d5d47008e77aa144f7852
charith <chariththarindu@gmail.com>
2020-01-03 22:17:06 +0530

    initial commit

---
+++ src/test/java/com/saviya/discovery/SaviyaAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.saviya.discovery;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaviyaAppDiscoveryServiceApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
